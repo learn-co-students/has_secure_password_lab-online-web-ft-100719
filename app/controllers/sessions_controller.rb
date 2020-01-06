b9709adba7db7130dc871cd75d48db3de7aeb5ad
github username <github email address>
2020-01-06 10:13:19 -0600

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,11 +3,12 @@ class SessionsController < ApplicationController
     def new
     end 
 
-    def create 
-        @user = User.find_by(strong_params(:name))    
-        return head(:forbidden) unless @user.authenticate(params[:user][:passwor])
-        session[:user_id] = @user.id 
-    end 
+    def create
+        @user = User.find_by(strong_params(:name))
+      return head(:forbidden) unless @user.authenticate(params[:user][:password])
+        session[:user_id] = @user.id
+  end
+
 
     def strong_params(*args)
         params.require(:user).permit(*args)
