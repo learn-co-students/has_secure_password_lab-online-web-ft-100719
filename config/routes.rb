59440b24057f7029aa3757ee6009cf66d7787e53
Kathleen Kamali <kathleenkamali@gmail.com>
2020-01-06 16:27:26 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/homepage', to: "users#home"
+  get '/signup', to: "users#new"
+  post '/signup', to: "users#create"
+  post '/login', to: "sessions#create"
 end
