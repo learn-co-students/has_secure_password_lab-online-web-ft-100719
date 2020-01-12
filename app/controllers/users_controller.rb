be2c41e7b8cbf3a2b337b739f124e08cc0cef40f
lmjcbs <hello@liamjacobs.dev>
2020-01-12 15:27:36 +0000

    Done.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  before_action :logged_in?, only: [:home]
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to controller: 'users', action: 'home'
+    else
+      flash[:error] = @user.errors.full_messages
+      redirect_to controller: 'users', action: 'new'
+    end
+  end
+
+  def home
+    @user = set_user
+    render 'index'
+  end
+
+  private
+
+  def set_user
+    User.find_by_id(session[:user_id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
+end
